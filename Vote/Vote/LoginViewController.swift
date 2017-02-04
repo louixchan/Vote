8e45d381e6bdc2326fc6c143fd984f24bace027a
Cliffgithub <Cliff@Chings-MacBook-Pro-3.local>
2017-02-04 21:25:39 +0800

    Ver2WIthSegueAndLoginViewControllerImplemented
    Still facing same error

---
+++ Vote/Vote/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Vote
+//
+//  Created by Ching Kim Fu Cliff on 2/4/17.
+//  Copyright © 2017 Louis. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import FirebaseDatabase
+import FirebaseAuth
+import FirebaseStorage
+
+class LoginViewController: UIViewController {
+    
+    @IBAction func LoadMainPage(unwindSegure: UIStoryboardSegue) {
+    }
+    
+    
+}
